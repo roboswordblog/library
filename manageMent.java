5c9872043481aa01974593f683127b4c07d6a77c
Robosword <32asingh@stratfordschools.net>
2026-04-01 13:46:00 -0700

    Create manageMent.java
    Make main.java, and make the scanners for it, and the template for it.

---
+++ manageMent.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.io.File; 
+
+
+class manageMent{
+  Scanner libraryScanner = new Scanner(new File("books.txt"));
+  Scanner userScanner = new Scanner(new File("users.txt"));
+  manageMent{
+    
+  }
+  void readBookList(){}
+  
+  void readUserList(){}
+}
