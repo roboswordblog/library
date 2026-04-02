7dd7ae36f1aceb9c430ed1cfe85c907b587ec2b5
Robosword <32asingh@stratfordschools.net>
2026-04-01 18:21:49 -0700

    Update manageMent.java
    Fix the entire structure, because the previous one was broken.

--- manageMent.java
+++ manageMent.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
-import java.io.File; 
+import java.io.File;
+import java.io.FileNotFoundException;
 
+class manageMent {
+    String libraryString = "";
+    String usersString = "";
 
-class manageMent{
-  String libraryString = "";
-  String usersString = "";
-  manageMent{
-    try (Scanner libraryScanner = new Scanner(new File("books.txt"));) { //
-        while (fileScanner.hasNextLine()) { //
-            String data = fileScanner.nextLine(); //
-            fileStringy += data;
-            fileStringy += "\n";
+    // Constructor
+    manageMent() {
+
+        // Read books.txt
+        try (Scanner libraryScanner = new Scanner(new File("books.txt"))) {
+            while (libraryScanner.hasNextLine()) {
+                String data = libraryScanner.nextLine();
+                libraryString += data + "\n";
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Books file not found.");
         }
-    } catch (FileNotFoundException e) {
-        System.out.println("An error occurred: File not found.");
-        e.printStackTrace();
-    }}
 
-  try (Scanner userScanner = new Scanner(new File("users.txt"));) { //
-        while (fileScanner.hasNextLine()) { //
-            String data = fileScanner.nextLine(); //
-            fileStringy += data;
-            fileStringy += "\n";
+        // Read users.txt
+        try (Scanner userScanner = new Scanner(new File("users.txt"))) {
+            while (userScanner.hasNextLine()) {
+                String data = userScanner.nextLine();
+                usersString += data + "\n";
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Users file not found.");
         }
-    } catch (FileNotFoundException e) {
-        System.out.println("An error occurred: File not found.");
-        e.printStackTrace();
-    }}
-  }
+    }
 
-  void readBookList(){}
-  
-  void readUserList(){}
+    void readBookList() {
+        System.out.println(libraryString);
+    }
+
+    void readUserList() {
+        System.out.println(usersString);
+    }
 }
